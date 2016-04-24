31162fdaa568b3ea2c055aaf01c05c76a8d46eab
thuraKadhim <thura.kadhim@gmail.com>
2016-04-24 10:53:15 +0200

    Check for Authentication

--- 30sep/Controllers/GenresController.cs
+++ 30sep/Controllers/GenresController.cs
@@ -36,6 +36,7 @@ namespace _30sep.Controllers
         }
 
         // GET: Genres/Create
+        [Authorize]
         public ActionResult Create()
         {
             return View();
@@ -59,6 +60,7 @@ namespace _30sep.Controllers
         }
 
         // GET: Genres/Edit/5
+        [Authorize]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -90,6 +92,7 @@ namespace _30sep.Controllers
         }
 
         // GET: Genres/Delete/5
+        [Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
