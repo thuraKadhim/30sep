d301d010060da66c2d97c33ff1741cb6e606f6c9
thuraKadhim <thura.kadhim@gmail.com>
2016-04-24 10:52:28 +0200

    only authenticated user can edit or delete Authors, Gernres and review . the Reivew can only be edited or deleted by the user who created it.

--- 30sep/Controllers/AuthorsController.cs
+++ 30sep/Controllers/AuthorsController.cs
@@ -36,6 +36,7 @@ namespace _30sep.Controllers
         }
 
         // GET: Authors/Create
+        [Authorize]
         public ActionResult Create()
         {
             return View();
@@ -59,6 +60,7 @@ namespace _30sep.Controllers
         }
 
         // GET: Authors/Edit/5
+        [Authorize]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -90,6 +92,7 @@ namespace _30sep.Controllers
         }
 
         // GET: Authors/Delete/5
+        [Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
